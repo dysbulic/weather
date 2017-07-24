cca3b0b23a53286ceb382dbaed4fa3025e954264
Will Holcomb <will@dhappy.org>
2017-07-24 14:37:09 -0400

    mocking rest-client response

--- test/models/reading_test.rb
+++ test/models/reading_test.rb
@@ -5,6 +5,9 @@ class ReadingTest < ActiveSupport::TestCase
   end
 
   test "loads data" do
+    response = stub(body: file_fixture('37618.json').read)
+    RestClient.expects(:get).returns(response)
+    
     reading = Reading.for(Location.find_or_create_by( zipcode: '37618' ))
   end
 end
